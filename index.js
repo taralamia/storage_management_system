8f898e55c61ab143168442bd3a989027a0444907
taralamia <79480117+taralamia@users.noreply.github.com>
2025-02-15 05:49:47 +0600

    Installed all the necessary packages

---
+++ index.js
@@ -0,0 +1,7 @@
+/*
+* Title: Storage Management System
+* Description: a Storage Management System while following the MVC design pattern. 
+* Author: Tabassum Tara Lamia
+* Date: 14/02/2025
+*/
+const mongoose = require('mongoose');
